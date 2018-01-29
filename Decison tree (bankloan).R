df80c750dc2cd8e265eca5fc6166450e35d7b54d
Akanksha Verma <akanksha_1912@yahoo.com>
2018-01-29 12:36:32 +0530

    Decision tree practice on bankloan dataset

--- Decison tree (bankloan).R
+++ Decison tree (bankloan).R
@@ -2,6 +2,7 @@
 # calling dataset
 bank <- read.csv("/Users/admin/Documents/Documents/JLU/4th Sem/Datasets/bankloan.csv")
 head(bank)
+set.seed(88)
 # test train split
 nrow(bank)
 700*0.80
@@ -21,8 +22,8 @@ plot(tree_bank); text(tree_bank)
 pred_bank<-predict(tree_bank,test_bank)
 round(pred_bank)
 #confusion matrix
-cm <- table(round(pred_bank),test_bank$default)
-cm
+cm_tree <- table(round(pred_bank),test_bank$default)
+cm_tree
 
 #creating random forest model
 rf_bank<-randomForest(default~age+ed+income,data=train_bank)
@@ -35,7 +36,22 @@ round(pred_rf_bank)
 cm_rf <- table(round(pred_rf_bank),test_bank$default)
 cm_rf
 
-cm
-
 #confusion matrix of tree model is better than random forest model
+
+#using train command
+tm_bank <- train(default~age+ed+income,data=train_bank,method="rpart")
+tm_bank
+#predicting test on train
+pred_tm_bank <- predict(tm_bank,test_bank)
+round(pred_tm_bank)
+#confusion matrix
+cm_tm <- table(round(pred_tm_bank),test_bank$default)
+cm_tm
+
+#comparing all three confusion matrix
+cm_tree
+
+cm_rf
+
+com_tm
 

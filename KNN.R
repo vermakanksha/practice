278db2edfe740af37632dc44f9caffe4152c47aa
Akanksha Verma <akanksha_1912@yahoo.com>
2018-02-11 21:53:04 +0530

    KNN

--- KNN.R
+++ KNN.R
@@ -29,3 +29,39 @@ wbcd_n <- as.data.frame(lapply(wbcd[2:31],
                                normalize))
 #confirm that normalization worked
 summary(wbcd_n$area_mean)
+
+#train n test split
+nrow(wbcd)
+569 * 0.80
+sample <- sample(569,455)
+train_wbcd <- wbcd[sample,-1]
+head(train_wbcd)
+test_wbcd <- wbcd[-sample,-1]
+train_wbcd=as.data.frame(train_wbcd)
+
+head(test_wbcd)
+train_target <- wbcd[sample,"diagnosis"]
+test_target <- wbcd[-sample,"diagnosis"]
+str(train_target)
+nrow(train_target)
+
+#applying knn from class
+library(class)
+#output <- knn(train = train_wbcd, test = test_wbcd, cl = train_target, k = 5)
+output <- knn(train = train_wbcd, test = test_wbcd, cl = train_target, k = 21)
+output
+
+#confusion matrix
+cm <- table(output,test_target)
+
+#accuracy from confusion matrix
+cv <- sum(diag(cm))
+cv
+total <- nrow(wbcd[-sample,])
+cv/total * 100
+
+#another way to create confusion matrix using CrossTable command
+#install.packages("gmodels")
+library(gmodels)
+
+CrossTable(x=test_target,y=output,prop.chisq = FALSE)
